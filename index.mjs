0735f7807dd3046a7c00db1cd37a5f629fcbe00f
Steeve Pommier <CostardRouge@users.noreply.github.com>
2024-03-26 07:00:01 +0100

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
 const PORT = process.env.PORT || 3333;
-const HOST = process.env.HOST || '127.0.0.1';
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.use(express.json());
 
